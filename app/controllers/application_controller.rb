7424a5ec18de9ae498d8277728c446fdc5c488ac
cam4994 <colin.a.mosley@gmail.com>
2020-10-22 13:42:08 -0500

    fixed application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   skip_forgery_protection
 
   def encode_token(payload)
-    JWT.encode(payload, "hello")
+    JWT.encode(payload, ENV["SECRET_API_KEY"])
   end
 
   def auth_header 
@@ -15,8 +15,7 @@ class ApplicationController < ActionController::Base
     if auth_header 
       token = auth_header.split(' ')[1]
       begin 
-        # ENV["SECRET_API_KEY"]
-        JWT.decode(token, "hello", true, algorithm: 'HS256')
+        JWT.decode(token, ENV["SECRET_API_KEY"], true, algorithm: 'HS256')
       rescue JWT::DecodeError
         []
       end
